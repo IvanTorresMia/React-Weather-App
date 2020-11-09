b505ec4a6839adeb964dcd42542bcd3aea7a7881
Ivan Torres <ivantorresmia27@gmail.com>
2020-11-08 17:24:47 -0800

    starting new react app

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+
+import './App.css'
+
+class App extends Component {
+  // This is a life cycle method, it is required to rended the component
+
+  // Here we have JSX which is very similar to html. and you can use javascript insde of here
+
+  // use className 
+  render() {
+    return (
+      <div className='App'>
+        <h1>App</h1>
+      </div>
+    )
+
+  }
+}
+
+export default App;
+
+
+
+
+
+
+// import './App.css';
+// import Todos from './components/Todos';
+
+// function App() {
+  
+//   return (
+//     <div className="App">
+//     <Todos />
+//     </div>
+//   );
+  
+// }
+
+// export default App;
+
+// This is JSX very similar to html. 
+// you can include js within here by using the curly braces. 
+
+
+// most of our components get called in here. 
+
+//Adding a state, we have to add a state that we are able to feed into different
+
+
+
