7a4a41f6c5ad461fbc5ee43d46de474e054d9df7
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-04 18:54:49 -0800

    Set up footer

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style.css'
+
+function Footer() {
+
+    return (
+        <div>
+        <header className="bg-dark p-2 fixed-buttom">
+          <div className="row">
+            <div className="col-lg text-center">
+              <h1 className="text-white">Footer</h1>
+            </div>
+          </div>
+        </header>
+      </div>
+    )
+}
+
+export default Footer;
