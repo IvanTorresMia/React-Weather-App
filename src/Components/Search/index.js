7063620989edf00d0cc615a277f26a8cb8799291
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-04 18:25:47 -0800

    starting search component

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Search() {
 
     return (
-        <div className="col rounded">
+        <div className="row rounded">
             <h1>Search a City!</h1>
-            <input type="text" className="bar form-control text-center" placeholder="What are you looking for?" onChange={} />
+            <input type="text" className="bar form-control text-center" placeholder="What are you looking for?" />
         </div>
     )
 }
