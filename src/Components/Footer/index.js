8bdbc91cf1bf51464e3f5cbf2f3ae97710428555
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-04 22:52:57 -0800

    Set the footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './style.css'
 
+
 function Footer() {
 
     return (
         <div>
-        <header className="bg-dark p-2 fixed-buttom">
+        <header className="bg-dark p-2 fixed-buttom footer">
           <div className="row">
             <div className="col-lg text-center">
               <h1 className="text-white">Footer</h1>
