600384c258698ccad458d4e2b0519e1e29447b3f
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-04 18:25:32 -0800

    starting history component

---
+++ src/Components/History/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function History() {
+
+    return (
+        <div className="row rounded">
+            {/* render buttons in here. */}
+            <button className="btn">California</button>
+            <button className="btn">California</button>
+        </div>
+    )
+}
+
+export default History;
