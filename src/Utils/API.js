9f42009fc01a24be1682f98edb704778e74f86fd
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-06 14:23:03 -0800

    testing API for weather.

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,2 +1,12 @@
 import axios from 'axios'
 
+const KEY = "58d666652ff1413e43798800e2896f32";
+
+const API = {
+    // Grab the current weather. 
+    getWeather: function(city) {
+        return axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + KEY + "&units=imperial")
+    }
+}
+
+
