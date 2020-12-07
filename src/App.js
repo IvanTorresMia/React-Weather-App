48fab17d2af638b5d59b44a74ad9fb6d093d7a1d
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-06 20:14:47 -0800

    setting up handle change

--- src/App.js
+++ src/App.js
@@ -1,85 +1,83 @@
-import React, { useState, useEffect } from 'react'
-import Nav from './Components/Nav'
-import WeatherContext from './Utils/WeatherContext'
-import Footer from './Components/Footer'
-import API from './Utils/API'
-import moment from 'moment'
-import Search from './Components/Search'
-import History from './Components/History'
-import Cards from './Components/Cards'
-import Weather from './Components/Weather'
+import React, { useState, useEffect } from "react";
+import Nav from "./Components/Nav";
+import WeatherContext from "./Utils/WeatherContext";
+import Footer from "./Components/Footer";
+import API from "./Utils/API";
+import moment from "moment";
+import Search from "./Components/Search";
+import History from "./Components/History";
+import Cards from "./Components/Cards";
+import Weather from "./Components/Weather";
 
-
-// use context 
+// use context
 // set the state for the current weather, put the value of the api
 
 function App() {
+  const [inputState, setInputState] = useState("");
+  const [currentWeather, setCurrentWeather] = useState({
+    name: "",
+    temperature: "",
+    humidity: "",
+    Windspeed: "",
+    UVindex: "",
+  });
 
-    const [currentWeather, setCurrentWeather] = useState({
-        name: "",
-        temperature: "",
-        humidity: "",
-        Windspeed: "",
-        UVindex: "",
-    })
+  //   make the axios calls, then set the state of the weather, once thats done.
+  // use the vales of the state to fill in the props of compenents i.e Weather
 
-//   make the axios calls, then set the state of the weather, once thats done. 
-// use the vales of the state to fill in the props of compenents i.e Weather
+  // practice back end
+  // save the weather data to a data base then call it back when a button is pressed
 
-// practice back end
-// save the weather data to a data base then call it back when a button is pressed
+  function handleInput(event) {
+    const { value } = event.target;
+    setInputState(value)
+  }
 
-
-
-function getWeather() {
+  function getWeather() {
     const currentDate = moment().format(" (dddd, MMMM Do YYYY)");
-    API.getWeather('California').then((res) => {
+    API.getWeather(inputState).then((res) => {
+      const long = res.data.coord.lon;
+      const latt = res.data.coord.lat;
+      API.getUV(latt, long).then((response) => {
+        const UVindexVal = response.data.value;
         setCurrentWeather({
-            ...currentWeather,
-            name: res.data.name + currentDate,
-            temperature: "Temperature: " + res.data.main.temp + "°",
-            humidity: "Humidity " + res.data.main.humidity + "%",
-            Windspeed: "Windspeed " + res.data.wind.speed + " MPH"
-        })
-       
-    })
-    console.log(currentWeather)
-}
+          ...currentWeather,
+          name: res.data.name + currentDate,
+          temperature: "Temperature: " + res.data.main.temp + "°",
+          humidity: "Humidity " + res.data.main.humidity + "%",
+          Windspeed: "Windspeed " + res.data.wind.speed + " MPH",
+          UVindex: UVindexVal,
+        });
+      });
+    });
+  }
 
-
-return (
-<div>
-<Nav />
-<WeatherContext.Provider value={currentWeather}>
-<div className="conatiner">
-            <div className="row">
-                <div className="col">
-                < Search handleSearch={getWeather}/>
-                    < History/>
-                </div>
-                <div className="col">
-                    < Weather
-                    name={currentWeather.name}
-                    temp={currentWeather.temperature}
-                    humidity={currentWeather.humidity}
-                    Windspeed={currentWeather.Windspeed}
-                    />
-                    < Cards/>
-                </div>
+  return (
+    <div>
+      <Nav />
+      <WeatherContext.Provider value={currentWeather}>
+        <div className="conatiner">
+          <div className="row">
+            <div className="col">
+              <Search handleSearch={getWeather} handleChange={handleInput} />
+              <History />
             </div>
+            <div className="col">
+              <Weather
+                name={currentWeather.name}
+                temp={currentWeather.temperature}
+                humidity={currentWeather.humidity}
+                Windspeed={currentWeather.W}
+                UVindex={currentWeather.UVindex}
+              />
+              <Cards />
+            </div>
+          </div>
         </div>
-</WeatherContext.Provider>
-< Footer/>
-</div>
-
-
-)
-
+      </WeatherContext.Provider>
+      <Footer />
+    </div>
+  );
 }
-
-
-
 
 export default App;
-
-
--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
 
-function Search({handleSearch}) {
-
-    return (
-        <div className="container rounded">
-            <div className="row">
-            <h1>Search a City!</h1>
-            </div>
-            <div className="row">
-            <input type="text" className="bar text-center" placeholder="What are you looking for?" />
-            <button className="btn" onClick={handleSearch}>Search</button>
-            </div>
-        </div>
-    )
+function Search({ handleSearch, handleChange }) {
+  return (
+    <div className="container rounded">
+      <div className="row">
+        <h1>Search a City!</h1>
+      </div>
+      <div className="row">
+        <input
+          type="text"
+          className="bar text-center"
+          placeholder="What are you looking for?"
+          onChange={handleChange}
+        />
+        <button className="btn" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
