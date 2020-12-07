f6f6e5c908f819ff1bff4550fcb59aea9289b774
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-06 20:15:02 -0800

    updating props

--- src/Components/Weather/index.js
+++ src/Components/Weather/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 // import WeatherContext from '../../Utils/WeatherContext'
-function Weather({ name, temp, humidity, Windspeed }) {
+function Weather({ name, temp, humidity, Windspeed, UVindex }) {
   return (
     <div>
       <h3>{name}</h3>
       <p>{temp}</p>
       <p>{humidity}</p>
       <p>{Windspeed}</p>
+      <p>{UVindex}</p>
     </div>
   );
 }
