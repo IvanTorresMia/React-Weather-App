37b1bd8fb144ee7eb370013046097e0cbd117dcf
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-06 21:12:57 -0800

    making props for 5 day forcast

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function Cards() {
-
-    return (
-        <div>
-            <h1>Cards Go Here</h1>
-        </div>
-    )
+function Cards({ temp, currentTime, humidity, iconImg }) {
+  return (
+    <div>
+      <h3>Five Day Forcast</h3>
+      <p>{currentTime}</p>
+      <p>{temp}</p>
+      <p>{humidity}</p>
+      <img src={iconImg} alt="weather image" />
+    </div>
+  );
 }
 
 export default Cards;
