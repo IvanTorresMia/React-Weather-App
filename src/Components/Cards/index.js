022401aa1fd6746f02e81857887feefddd01140c
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-07 13:17:55 -0800

    mapping through data

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import WeatherContext from "../../Utils/WeatherContext";
 
-function Cards({ temp, currentTime, humidity, iconImg }) {
+function Cards() {
+  const context = useContext(WeatherContext);
+
   return (
     <div>
       <h3>Five Day Forcast</h3>
-      <p>{currentTime}</p>
-      <p>{temp}</p>
-      <p>{humidity}</p>
-      <img src={iconImg} alt="weather image" />
+      {context.map((day, i) => (
+        <>
+          <p key={i}>{day.dt_txt.substring(0, 10)}</p>
+          <p>Temperature {day.main.temp} °</p>
+          <p>Humidity {day.main.humidity} %</p>
+          <img src={"https://openweathermap.org/img/wn/" + day.weather[0].icon + "@2x.png"} />
+        </>
+      ))}
     </div>
   );
 }
