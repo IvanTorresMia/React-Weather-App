344cbd83f9c1d3b02bac31c65fafc9f42b6ee5f6
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-07 21:55:41 -0800

    styled nav

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -7,7 +7,7 @@ function Nav() {
       <header className="p-2 container-fluid Nav">
         <div className="row">
           <div className="col-lg text-center">
-            <h1 className="text">My Weather App</h1>
+            <h1 className="display-4">My Weather App</h1>
           </div>
         </div>
       </header>
