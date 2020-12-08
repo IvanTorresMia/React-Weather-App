7cc3d35b949afda016675f72c9e1b4626d6cd5f6
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-07 23:10:46 -0800

    added server

---
+++ client/src/Utils/API.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const KEY = "58d666652ff1413e43798800e2896f32";
+
+const API = {
+    // Grab the current weather. 
+    getWeather: function(city) {
+        return axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + KEY + "&units=imperial")
+    },
+    getUV: function(latt, long) {
+        return axios.get("https://api.openweathermap.org/data/2.5/uvi?lat=" + latt + "&lon=" +long + "&appid=" + KEY)
+    },
+    get5Day: function(city) {
+        return axios.get("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + KEY +  "&units=imperial")
+    }
+}
+
+
+export default API;
+
