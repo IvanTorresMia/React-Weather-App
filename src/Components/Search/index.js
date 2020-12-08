5435bf387cf614a104f748f740308835d842b873
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-07 22:52:21 -0800

    styling to search component

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,22 +1,20 @@
 import React from "react";
+import "./style.css";
 
 function Search({ handleSearch, handleChange }) {
   return (
-    <div className="container rounded">
-      <div className="row">
-        <h1>Search a City!</h1>
-      </div>
-      <div className="row">
-        <input
-          type="text"
-          className="bar text-center"
-          placeholder="What are you looking for?"
-          onChange={handleChange}
-        />
-        <button className="btn" onClick={handleSearch}>
-          Search
-        </button>
-      </div>
+    <div className="rounded m-4 p-4 search">
+      <h1 className="search-title">Search a City!</h1>
+
+      <input
+        type="text"
+        className="bar text-center p-2"
+        placeholder="City Name"
+        onChange={handleChange}
+      />
+      <button className="btn ml-2" onClick={handleSearch}>
+        Search
+      </button>
     </div>
   );
 }
