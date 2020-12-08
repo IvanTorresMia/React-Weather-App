806aa7828d40176a594b13d3eba65a795daa9b8f
Ivan Torres <ivantorresmia27@gmail.com>
2020-12-07 22:52:06 -0800

    adding styling to history component

--- src/Components/History/index.js
+++ src/Components/History/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import './style.css'
 
 function History() {
 
     return (
-        <div className="row rounded">
+        <div className="rounded mr-4 ml-4 p-4 history">
             {/* render buttons in here. */}
+            <div className="row m-2">
             <button className="btn">California</button>
+            </div>
+            <div className="row m-2">
             <button className="btn">California</button>
+            </div>
         </div>
     )
 }
